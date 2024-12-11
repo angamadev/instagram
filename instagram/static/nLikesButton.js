7c57711417b3f6daf63fffa1e35ba76af8d0fadd
Andres Garcia <152220231+angamadev@users.noreply.github.com>
2024-12-11 06:23:21 +0000

    Actualizar nLikesButton.js
    Limpiar codigo sobrante

--- instagram/static/nLikesButton.js
+++ instagram/static/nLikesButton.js
@@ -34,38 +34,3 @@ document.addEventListener("DOMContentLoaded",() => {
         })
     })
 })
-// Archivo JS para los Botones de me gusta con los iconos incluidos
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     console.log('DOM cargado');
-//     // Selecciono todos los botones de me gusta
-//     const likeButtons = document.querySelectorAll('a.likeButton');
-//     // De cada uno de ellos cojo su Span y modifico su valor de Likes y cambio el icono si corresponde
-//     likeButtons.forEach(likeButton => {
-//         let nLikesSpan = likeButton.querySelector('span');
-//         let nLikes = parseInt(nLikesSpan.innerHTML);
-//         likeButton.addEventListener('click', (event) => {
-
-//         // Prevenimos el comportamiento por defecto del botón
-//         event.preventDefault();
-
-//         // Hacemos una petición GET al href del botón
-//         fetch(likeButton.href)
-//             .then(response => response.json())
-//             .then(data => {
-
-//             // Si la petición fue exitosa, cambiamos el contenido del botón
-//             if (data.liked) {
-//                 nLikes += 1;
-//                 likeButton.innerHTML = `<span>${nLikes}</span> <i class="bi bi-suit-heart-fill"></i>`;
-//             }
-//             // Si la petición fue Falsa, cambiamos el contenido del botón
-//             else {
-//                 nLikes -= 1;
-//                 likeButton.innerHTML = `<span>${nLikes}</span> <i class="bi bi-heart"></i>`;
-//             }
-//         })
-//         .catch(error => console.error('Error:', error));
-//         })
-//     })
-// })
